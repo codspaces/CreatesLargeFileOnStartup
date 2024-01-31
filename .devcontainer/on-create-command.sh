c5424d4e3804fdbbd7f7f0457ce0202716565f8f
Alexa Gillman <alexagillman@microsoft.com>
2024-01-31 15:14:14 +0000

    add zip

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
 
+# Create a large file 
 dd if=/dev/zero of=large_file.txt bs=1M count=1000
+
+# Create a large tarball 
+dd if=/dev/zero of=file_to_zip bs=1M count=4096
+tar -cvzf large_zip_file.tar.gz file_to_zip
+rm file_to_zip
